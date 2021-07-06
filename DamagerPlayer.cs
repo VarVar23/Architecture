cdaf8f124751992a3de0079f0ed0337cc421350d
NooB <oleg.nakhaevgame@mail.ru>
2021-07-06 22:20:56 +0300

    HW 2

---
+++ DamagerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+class DamagerPlayer : MonoBehaviour, IDamager
+{
+
+    private void Start()
+    {
+        
+    }
+    public void Damage(Player player, int damage)
+    {
+        player.Hp -= damage;
+
+        if(player.Hp <= 0)
+        {
+            Death(player);
+        }
+    }
+
+    private void Death(Player player) =>
+        Destroy(player);
+
+}
+
