aa113004aff03a379c71b84b5b5ac68471152e1f
NooB <oleg.nakhaevgame@mail.ru>
2021-07-06 22:18:09 +0300

    HW 2

---
+++ MoveTransform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+class MoveTransform : IMove
+{
+    private Rigidbody2D _rigidBody;
+    private Vector3 _move;
+    public float Speed { get; protected set; }
+
+    public MoveTransform(Rigidbody2D rigidbody, float speed)
+    {
+        _rigidBody = rigidbody;
+        Speed = speed;
+    }  
+
+    public void Move(float horizontal, float vertical, float deltaTime)
+    {
+        var speed = Speed * deltaTime;
+        _move.Set(horizontal * speed, vertical * speed, 0);
+        _rigidBody.AddForce(_move);
+    }
+}
+
---
+++ Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _acceliration;
+    [SerializeField] private Rigidbody2D _bullet;
+    [SerializeField] private Transform _barrel;
+    [SerializeField] private float _force;
+    [SerializeField] private int _damage;
+    private Rigidbody2D _rigidBody;
+    private IMove _moveTransform;
+    private IInput _inputManager;
+    private IShoot _shoot;
+    private IDamager _damager;
+
+    public float Hp;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _moveTransform = new AccelerationMove(_rigidBody, _speed, _acceliration);
+    }
+
+    private void Update()
+    {
+        _moveTransform.Move(_inputManager.Horizontal, _inputManager.Vertical, _inputManager.DeltaTime);
+        
+        if(_inputManager.Fire1)
+        {
+            _shoot.ShootBullet(_rigidBody, _barrel);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) =>
+        _damager.Damage(this, _damage);
+        
+    public void InputManager(IInput input) =>
+        _inputManager = input;
+
+    public void Shoot(IShoot shoot) =>
+        _shoot = shoot;
+
+    public void Damager(IDamager damager) =>
+        _damager = damager;
+}
